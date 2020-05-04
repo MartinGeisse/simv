cffde343dcb0a0bf864654f3cc9c2e1bf1ab2093
Martin Geisse <martingeisse@googlemail.com>
2020-05-04 13:32:13 +0200

    more tests

--- src/test/java/name/martingeisse/simv/cpu/BasicInstructionTest.java
+++ src/test/java/name/martingeisse/simv/cpu/BasicInstructionTest.java
@@ -114,4 +114,46 @@ public class BasicInstructionTest {
         Assert.assertEquals(37, cpu.getRegister(3));
     }
 
+    @Test
+    @Program({
+            "addi x1, x0, 42",
+            "sll x1, x1, 1",
+            "addi x2, x0, 42",
+            "sll x2, x2, 2",
+            "addi x3, x0, 3",
+            "sll x3, x3, 30",
+            "addi x4, x0, 3",
+            "sll x4, x4, 31",
+            "addi x5, x0, 6",
+            "sll x5, x5, 31",
+            "addi x6, x0, 42",
+            "sll x6, x6, 0",
+    })
+    public void testShiftLeftImmediate() {
+        execute();
+        testMachine.assertRegistersIntactExcept(1, 2, 3, 4, 5, 6);
+        Assert.assertEquals(84, cpu.getRegister(1));
+        Assert.assertEquals(168, cpu.getRegister(2));
+        Assert.assertEquals(0xc0000000, cpu.getRegister(3));
+        Assert.assertEquals(0x80000000, cpu.getRegister(4));
+        Assert.assertEquals(0, cpu.getRegister(5));
+        Assert.assertEquals(42, cpu.getRegister(6));
+    }
+
+    @Test
+    @Program({
+            "addi x1, x0, 42",
+            "addi x2, x0, 2",
+            "sll x2, x1, x2",
+            "addi x3, x0, 32",
+            "sll x3, x1, x3",
+    })
+    public void testShiftLeft() {
+        execute();
+        testMachine.assertRegistersIntactExcept(1, 2, 3);
+        Assert.assertEquals(42, cpu.getRegister(1));
+        Assert.assertEquals(168, cpu.getRegister(2));
+        Assert.assertEquals(42, cpu.getRegister(3));
+    }
+
 }
